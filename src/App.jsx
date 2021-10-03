0803472d33651b8de3cde69695737d3d6d5e6b9a
Mosaddik Ali Maruf <malinjr07@gmail.com>
2021-10-03 23:08:09 +0600

    Design Left

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,46 @@
-import React from "react";
+import React, {useState} from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Card, Col, Container, Form, Row, Button } from 'react-bootstrap';
+import {
+    Card,
+    Col,
+    Container,
+    Form,
+    Row,
+    Button
+} from 'react-bootstrap';
 import './App.css';
 import Upload from "./drag-n-drop/upload";
 
 function App() {
-  return (
-    <Container className='mt-5'>
-      <Row className='justify-content-center'>
-        <Col md={6}>
-          <Card>
-            <Card.Header>Custom Image DropBox</Card.Header>
-            <Card.Body>
-              <Form action='' encType="multipart/form-data">
-              <Upload/>
-                <Button variant="primary" type="submit">
-                  Submit
-                </Button>
-              </Form>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
-  );
+    const [uploads,
+        setUploads] = useState({
+          images:[]
+        })
+    const submission = (s) => {
+        s.preventDefault()
+    }
+    const childData=(d)=>{
+      setUploads({ ...uploads, images: d})
+    }
+    return (
+        <Container className='mt-5'>
+            <Row className='justify-content-center'>
+                <Col md={6}>
+                    <Card>
+                        <Card.Header>Custom Image DropBox</Card.Header>
+                        <Card.Body>
+                            <Form action='' onSubmit={submission} encType="multipart/form-data">
+                              <Upload many pushUp={childData} accept=".jpg,.png,.jpeg" multiple />
+                              <Button variant="primary" type="submit">
+                                  Submit
+                              </Button>
+                            </Form>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
 
 export default App;
--- src/drag-n-drop/upload.jsx
+++ src/drag-n-drop/upload.jsx
@@ -3,18 +3,47 @@ import { Form } from 'react-bootstrap'
 import './upload.css'
 const convertSize=(k)=>Math.round(k/1000)
 
-function Upload({maxSize=500000,pushUp,many}) {
+function Upload({maxSize=500000,pushUp,...others}) {
     const [images, allImages] = useState({})
 
     useEffect(() => {
     }, [images])
 
-    const uploadFile=(f)=>{
-        const newFile  = f.target.files; //Please Explain This line
-        console.log(newFile);
-        if (newFile.length) {
-            
+    const newImage=(n)=>{
+        for (const i of n) {
+            if (i.size<maxSize) {
+                if (!others.multiple) {
+                    return{i}
+                }
+                images[i.name]=i; //Explain this line
+            }
         }
+        return { ...images };
+    }
+    
+    const objToArr=(o)=>{
+        Object.keys(o).map((i)=>o[i])
+    }
+
+    const pushArr=(p)=>{
+        const arrFiles=objToArr(p)
+        pushUp(arrFiles);
+    }
+
+    const uploadFile=(f)=>{
+        const uploaded  = f.target.files; 
+        if (uploaded.length) {
+            
+            const completeUpload= newImage(uploaded);
+            allImages(completeUpload)
+            pushArr(completeUpload)
+        }
+    }
+
+    const remove=(e)=>{
+        delete images[e];
+        allImages({...images})
+        pushArr({...images});
     }
     
     return (
@@ -22,7 +51,7 @@ function Upload({maxSize=500000,pushUp,many}) {
             <section className='bigContainer containers' >
             <Form.Group className="mb-3">
                 <Form.Label htmlFor='file-upload' > Click or Drag Images here </Form.Label>
-                <input id='file-upload' className='mainBox uploader' title='' value='' onChange={uploadFile} type="file" />
+                <input id='file-upload' className='mainBox uploader' title='' value=''  onChange={uploadFile} type="file" {...others}/>
             </Form.Group>
             </section>
 
@@ -31,22 +60,21 @@ function Upload({maxSize=500000,pushUp,many}) {
                 <ul className='upload-area' >
                     {Object.keys(images).map((fileName, index) => {
                         let file = images[fileName];
-                        console.log('what is file in line 23', file);
                         let isImageFile = file.type.split("/")[0] === "image";
                         return (
                             <li key={index}>
                                 <div>
                                     {isImageFile && (
-                                        <img
+                                        <img className='preview'
                                             src={URL.createObjectURL(file)}
                                             alt={`file preview ${index}`}
                                         />
                                     )}
-                                    <div isImageFile={isImageFile}>
+                                    <div>
                                         <span>{file.name}</span>
                                         <aside>
                                             <span>{convertSize(file.size)} kb</span>
-                                            <i className="fas fa-trash-alt" />
+                                            <i className="fas fa-trash-alt" onClick={()=>remove(fileName)} />
                                         </aside>
                                     </div>
                                 </div>
@@ -56,11 +84,12 @@ function Upload({maxSize=500000,pushUp,many}) {
                     <li className="mini-container containers">
                         <Form.Group>
                         <Form.Label htmlFor='mini-file-upload' > Click or Drag Images here </Form.Label>
-                            <input id='mini-file-upload' className='miniBox uploader' onChange={uploadFile}  title='' value='' type="file" />
+                            <input id='mini-file-upload' {...others} className='miniBox uploader' onChange={uploadFile}  title='' value='' type="file" />
                         </Form.Group>
                     </li>
                 </ul>
             </section>
+       
         </>
     )
 }
