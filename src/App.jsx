f3971709a119fac453989cc6e2f6aed04eae9249
Mosaddik Ali Maruf <malinjr07@gmail.com>
2021-10-03 09:08:27 +0600

    sass added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Card, Col, Container, Row } from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
-    <></>
+    <Container className='mt-5'>
+      <Row className='justify-content-center'>
+        <Col md={6}>
+          <Card>
+            <Card.Header>Custom Image DropBox</Card.Header>
+            <Card.Body>
+              
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
